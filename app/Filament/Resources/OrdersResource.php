cb38d5333a2515c73eaafafd2b57f35ea07167d6
webNekit <mochalov-hunter@mail.ru>
2024-11-13 19:21:17 +0300

    update

--- app/Filament/Resources/OrdersResource.php
+++ app/Filament/Resources/OrdersResource.php
@@ -5,9 +5,11 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\OrdersResource\Pages;
 use App\Filament\Resources\OrdersResource\RelationManagers;
 use App\Models\Order;
+use App\Models\Product;
 use Filament\Forms;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
@@ -15,11 +17,14 @@ use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Number;
 
 class OrdersResource extends Resource
 {
@@ -111,22 +116,45 @@ class OrdersResource extends Resource
                                     ->relationship("product", "name")
                                     ->preload()
                                     ->searchable()
-                                    ->required(),
+                                    ->reactive()
+                                    ->required()
+                                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('unit_amount', Product::find($state)?-> price ?? 0 ))
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('total_amount', Product::find($state)?-> price ?? 0 )),
                                 TextInput::make("quantity")
                                     ->label('Укажите кол-во')
                                     ->numeric()
                                     ->default(1)
                                     ->minValue(1)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set, Get $get) => $set('total_amount', $state * $get('unit_amount')))
                                     ->required(),
                                 TextInput::make("unit_amount")
                                     ->numeric()
+                                    ->label('Цена за шт.')
                                     ->disabled()
                                     ->required(),
                                 TextInput::make("total_amount")
+                                    ->label('Общая сумма')
                                     ->numeric()
+                                    ->disabled()
                                     ->required(),
                             ])
                     ])->columnSpanFull(),
+
+                    Placeholder::make('grand_total')
+                        ->label('Общая сумма заказа')
+                        ->content(function (Get $get, Set $set){
+                            $total = 0;
+                            if(!$repetears = $get('items')) {
+                                return $total;
+                            }
+                            foreach($repetears as $key => $repetear) {
+                                $total += $get("items.{$key}.total_amount");
+                            }
+
+                            return Number::currency($total, 'RUB');
+                        }),
                 ]),
             ]);
     }
