8016bbb97b45ac88a80f0c441b9fb959ab49cbd4
webNekit <mochalov-hunter@mail.ru>
2024-12-04 19:30:00 +0300

    update

--- app/Livewire/PageProducts.php
+++ app/Livewire/PageProducts.php
@@ -2,14 +2,52 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 #[Title('Товары')]
 class PageProducts extends Component
 {
+
+    #[Url]
+    public $selected_categories = [];
+
+    #[Url]
+    public $selected_brands = [];
+
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $price_range = 1000000;
+
+    #[Url]
+    public $sort = 'latest';
+
     public function render()
     {
-        return view('livewire.page-products');
+        $products = Product::query()->where("is_active", 1);
+
+        if (!empty($this->selected_categories)) {
+            $products->whereIn("category_id", $this->selected_categories);
+        }
+
+        if(!empty($this->selected_brands)) {
+            $products->whereIn("brand_id", $this->selected_brands);
+        }
+
+        if($this->featured) {
+            $products->where("is_featured", 1);
+        }
+
+        return view('livewire.page-products', [
+            'products' => $products->paginate(15),
+            'categories' => Category::where("is_active", 1)->get(["id", "name", "slug"]),
+            'brands' => Brand::where("is_active", 1)->get(["id", "name", "slug"]),
+        ]);
     }
 }
--- resources/views/livewire/page-products.blade.php
+++ resources/views/livewire/page-products.blade.php
@@ -1,3 +1,55 @@
-<div>
-    Страница товаров
+<div class="max-w-7xl mx-auto px-4">
+    <div class="w-full py-5">
+        Фильтры
+        <div class="w-full">
+            {{-- Фильтр категорий --}}
+            <div class="">
+                <div class="text-lg font-semibold uppercase text-gray-800">Категории</div>
+                @foreach ($categories as $category)
+                    <label wire:key="{{ $category->id }}" for="{{ $category->slug }}">
+                        <input wire:model.live="selected_categories" type="checkbox" id="{{ $category->slug }}" value="{{ $category->id }}">
+                        <span>{{ $category->name }}</span>
+                    </label>
+                @endforeach
+            </div>
+            {{-- Фильтр по брендам --}}
+            <div class="">
+                <div class="text-lg font-semibold uppercase text-gray-800">Бренды</div>
+                @foreach ($brands as $brand)
+                <label for="{{ $brand->slug }}">
+                    <input wire:model.live="selected_brands" type="checkbox" id="{{ $brand->slug }}" value="{{ $brand->id }}">
+                    <span>{{ $brand->name }}</span>
+                </label>
+                @endforeach
+            </div>
+            {{-- Фильтр по популярности --}}
+            <div class="">
+                <div class="text-lg font-semibold uppercase text-gray-800">Популярный товар</div>
+                <label for="is_featured">
+                    <input wire:model.live="featured" type="checkbox" id="is_featured" value="1">
+                    <span>Да</span>
+                </label>
+            </div>
+            {{-- Фильтр по цене --}}
+            <div class="">
+                <div class="text-lg font-semibold uppercase text-gray-800">Цена</div>
+                <label for="">
+                    <input type="range" max="1000000" value="12000" step="30000" id="" value="">
+                    <span>1000</span>
+                </label>
+            </div>
+            {{-- сортировка --}}
+            <div class="">
+                <div class="text-lg font-semibold uppercase text-gray-800">Сортировать по</div>
+                <select name="" id="">
+                    <option value="latest">Сначала новые</option>
+                    <option value="price">Сначала недорогие</option>
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="w-full py-5">
+        Товары
+        <x-product.product-group :products="$products" />
+    </div>
 </div>
