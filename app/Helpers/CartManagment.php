2403cdd161f854e1846c2694eb481a1ad9b248a4
webNekit <mochalov-hunter@mail.ru>
2024-12-17 19:09:21 +0300

    Добавили прибавление и удаление кол-во одного товара

--- app/Helpers/CartManagment.php
+++ app/Helpers/CartManagment.php
@@ -2,6 +2,7 @@
 
 namespace App\Helpers;
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Cookie;
 
 class CartManagment {
@@ -17,6 +18,24 @@ class CartManagment {
                 break;
             }
         }
+
+        if($existing_item !== null) {
+            $cart_items[$existing_item]['quantity']++;
+            $cart_items[$existing_item]['total_amount'] = $cart_items[$existing_item]['quantity'] * $cart_items[$existing_item]['unit_amount'];
+        } else {
+            $product = Product::where("id", $product_id)->first(['id', 'name', 'price', 'images']);
+            if ($product) {
+                $cart_items[] = [
+                    'product_id' => $product->name,
+                    'image' => $product->images[0],
+                    'quantity' => 1,
+                    'unit_amount' => $product->price,
+                    'total_amount' => $product->price
+                ];
+            }
+        }
+
+        self::addCartItemsToCookie($cart_items);
     }
 
     // добавление записей в куки
@@ -37,6 +56,38 @@ class CartManagment {
             $cart_items = [];
         }
 
+        return $cart_items;
+    }
+
+    // Привление кол-ва товара в корзине
+    static public function incrementQuantityToCartItem($product_id)
+    {
+        $cart_items = self::getCartItemsFromCookie();
+
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
+                $cart_items[$key]['quantity']++;
+                $cart_items[$key]['total_amount'] = $cart_items[$key]['quantity'] * $cart_items[$key]['unit_amount'];
+            }
+        }
+
+        self::addCartItemsToCookie($cart_items);
+        return $cart_items;
+    }
+
+    // удаление кол-ва товаров в корзине
+    static public function decrementQuantityToCartItem($product_id)
+    {
+        $cart_items = self::getCartItemsFromCookie();
+        foreach ($cart_items as $key => $item) {
+            if($item['product_id'] == $product_id) {
+                if($cart_items[$key]['quantity'] > 1) {
+                    $cart_items[$key]['quantity']--;
+                    $cart_items[$key]['total_amount'] = $cart_items[$key]['quantity'] * $cart_items[$key]['unit_amount'];
+                } 
+            }
+        }
+        self::addCartItemsToCookie($cart_items);
         return $cart_items;
     }
 }
